39cf26a9afdae1513a583115f8457bc14956d999
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-09-03 13:43:45 -0400

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Bobby Store</title>
+      </Head>
+      <h1>Store for Bobby</h1>
+    </div>
+  );
+}
