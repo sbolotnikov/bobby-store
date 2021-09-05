0ae07643df6901fa7c2a7eb15fc71a21949ec7cd
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-09-05 04:20:31 -0400

    deployment of front end fix next/image

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,4 +1,4 @@
-import Image from "next/Image";
+import Image from "next/image";
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid"
 import Currency from "react-currency-formatter"
@@ -13,8 +13,7 @@ function Product({ id, title, price, description, category, image }) {
         <div className="relative flex flex-col m-5 bg-white z-30 p-10">
             <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
             
-            {/* <Image src={image} height={200} width={200} objectFit="contain" /> */}
-            <Image src={'/images/image1.webp'} height={200} width={200} objectFit="contain" />
+            <Image src={image} height={200} width={200} objectFit="contain" />
             <h4 className="my-3">{title}</h4>
             
             <div className="flex">
