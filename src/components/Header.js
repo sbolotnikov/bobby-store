33fb0db73abe3a6f9e635590ab10bc8d53e8afd7
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-09-06 14:38:00 -0400

    Authentication and start of Shopping Cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,22 @@ import {
     SearchIcon,
     ShoppingCartIcon,
 } from "@heroicons/react/outline"
+import { signIn, signOut, useSession} from "next-auth/client";
+import { useRouter} from "next/router"
+
 const Header = () => {
+    const [session]=useSession();
+    const router = useRouter();
+
+
     return (
         <header>
             {/*Top navigation */}
             <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
                 <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
-                    <Image src={"/images/danzworldlogo.jpg"}
+                    <Image 
+                        onClick={() =>router.push('/')}
+                        src={"/images/danzworldlogo.jpg"}
                         width={150}
                         height={40}
                         objectFit="contain"
@@ -25,8 +34,10 @@ const Header = () => {
                 </div>
                 {/* Right side */}
                 <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-                    <div className="link">
-                        <p>Hello Bobby Tan</p>
+                    <div onClick={!session ? signIn : signOut} className="cursor-pointer link">
+                        <p className="hover:underline">
+                            {session ? `Hello, ${session.user.name}!` :`Sign In`}
+                        </p>
                         <p className="font-extrabold md:text-sm">Acoount & Lists</p>
 
                     </div>
@@ -34,7 +45,7 @@ const Header = () => {
                         <p>Returns</p>
                         <p className="font-extrabold md:text-sm">& Orders</p>
                     </div>
-                    <div className="relative flex items-center link">
+                    <div onClick={() =>router.push('/checkout')} className="relative flex items-center link">
                         <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
                         <ShoppingCartIcon className="h-10" />
                         <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
---
+++ src/styles/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAJy0b-Fp1qvtaH0DjYXXJJOljI-0K-XAE",
+    authDomain: "bobby-store.firebaseapp.com",
+    projectId: "bobby-store",
+    storageBucket: "bobby-store.appspot.com",
+    messagingSenderId: "142978659816",
+    appId: "1:142978659816:web:7dc51db5aa478c2a5e605d"
+  };
