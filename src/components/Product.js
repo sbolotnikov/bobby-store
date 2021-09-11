7c9d9b0dc45081a948f8e46d17f1732cd68990fe
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-09-11 02:12:01 -0400

    basket without grid and subtotal

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,13 +2,31 @@ import Image from "next/image";
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid"
 import Currency from "react-currency-formatter"
+import { useDispatch} from "react-redux"
+import { addToBasket } from "../slices/basketSlice";
 const MIN_RATING = 1;
 const MAX_RATING = 5;
 function Product({ id, title, price, description, category, image }) {
+    const dispatch = useDispatch();
+
     const [rating] = useState(
         Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1) + MIN_RATING)
     );
     const [hasPrime]=useState(Math.random()<0.5);
+
+    const addItemToBasket = () => {
+        const product = {
+            id, 
+            title, 
+            price,
+            rating,
+            description, 
+            category, 
+            image,
+            hasPrime,
+        }
+        dispatch(addToBasket(product));
+    }
     return (
         <div className="relative flex flex-col m-5 bg-white z-30 p-10">
             <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
@@ -22,7 +40,6 @@ function Product({ id, title, price, description, category, image }) {
                     .map((_, i) => (
                         <StarIcon className="h-5 text-yellow-500" />
                     ))}
-
             </div>
             <p className="text-xs my-2 line-clamp-2">{description}</p>
             <div className="mb-5">
@@ -30,12 +47,12 @@ function Product({ id, title, price, description, category, image }) {
             </div>
             {hasPrime && (
                 <div className="flex items-center space-x-2 mt-5">
-                    <img className="w-12" src="images/free-delivery-png-2.png" alt="free delivery" />
+                    <img className="w-12" loading="lazy" src="images/free-delivery-png-2.png" alt="free delivery" />
                     <p className="text-xs text-gray-500">Free Next-day delivery</p>
                 </div>
             )}
 
-            <button className="mt-auto button">Add to Basket</button>
+            <button className="mt-auto button" onClick={addItemToBasket}>Add to Basket</button>
         </div>
     )
 }
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import Header from "../components/Header"
+import CheckoutProduct from "../components/CheckoutProduct"
+import { useSelector } from "react-redux"
+import { selectItems } from "../slices/basketSlice";
+import Currency from "react-currency-formatter"
+import { useSession} from "next-auth/client"
 
 function Checkout() {
+    const items = useSelector(selectItems);
+    const [session] = useSession();
     return (
         <div className="bg-gray-100">
             <Header />
             <main className="lg:flex max-w-screen-2xl mx-auto">
                 {/* Left side */}
-                <div>
+                <div className="flex-grow m-5 shadow-sm">
                     <Image
                         src={"/images/danzworldlogo.jpg"}
                         width={1020}
@@ -16,19 +23,45 @@ function Checkout() {
                     />
 
                     <div className="flex flex-col p-5 space-y-10 bg-white">
-                        <h1 className="text-3xl border-b pb-4">Your Shopping Basket</h1>
-
+                        <h1 className="text-3xl border-b pb-4">
+                            {items.length === 0 ? "Your Shopping Basket is empty" : "Your Shopping Basket"}
+                        </h1>
+                        {items.map((item, i) =>
+                            <CheckoutProduct
+                                key={item.id}
+                                id={item.id}
+                                title={item.title}
+                                price={item.price}
+                                rating={item.rating}
+                                description={item.description}
+                                category={item.category}
+                                image={item.image}
+                                hasPrime={item.hasPrime}
+                            />
+                        )}
                     </div>
-
-
-
-
                 </div>
 
                 {/* Right side */}
-                <div>
-
-
+                <div className="flex flex-col bg-white p-10 shadow-md">
+                {items.length>0 && (
+                    <>
+                    <h2>
+                        Subtotal ({items.length} items):
+                        <span>
+                        {/* <Currency quantity={total} currency="USD" /> */}
+                        </span>
+                    </h2>
+                    <button 
+                    disabled={!session}
+                    className={`button mt-2 ${
+                        !session && 
+                        "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
+                    }`}>
+                        {!session ? "Sign in to checkout" :"Proceed to checkout"}
+                    </button>
+                    </>
+                )}
                 </div>
 
             </main>
