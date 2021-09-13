d106096d33d9146384efa6fd60cfbd396e977f5e
Sergey Bolotnikov <sbolotnikov@gmail.com>
2021-09-13 11:59:18 -0400

    backend, stripe implimentation webhook

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -4,12 +4,36 @@ import CheckoutProduct from "../components/CheckoutProduct"
 import { useSelector } from "react-redux"
 import { selectItems, selectTotal } from "../slices/basketSlice";
 import Currency from "react-currency-formatter"
-import { useSession} from "next-auth/client"
+import { useSession } from "next-auth/client"
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
     const items = useSelector(selectItems);
     const total = useSelector(selectTotal);
     const [session] = useSession();
+
+    const createCheckoutSession = async () => {
+        const stripe = await stripePromise;
+
+        // call back end to create a checkout session 
+        const checkoutSession = await axios.post('/api/create-checkout-session',
+            {
+                items: items,
+                email: session.user.email
+            });
+
+        // Redirect customer to Stripe Checkout
+        const result = await stripe.redirectToCheckout({
+            sessionId: checkoutSession.data.id,
+        })
+        
+        if (result.error) {
+            alert(result.error.message);
+        }
+    }
+
     return (
         <div className="bg-gray-100">
             <Header />
@@ -45,24 +69,25 @@ function Checkout() {
 
                 {/* Right side */}
                 <div className="flex flex-col bg-white p-10 shadow-md">
-                {items.length>0 && (
-                    <>
-                    <h2 className="whitespace-nowrap">
-                        Subtotal ({items.length} items):{" "}
-                        <span className="font-bold">
-                        <Currency quantity={total} currency="USD" />
-                        </span>
-                    </h2>
-                    <button 
-                    disabled={!session}
-                    className={`button mt-2 ${
-                        !session && 
-                        "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
-                    }`}>
-                        {!session ? "Sign in to checkout" :"Proceed to checkout"}
-                    </button>
-                    </>
-                )}
+                    {items.length > 0 && (
+                        <>
+                            <h2 className="whitespace-nowrap">
+                                Subtotal ({items.length} items):{" "}
+                                <span className="font-bold">
+                                    <Currency quantity={total} currency="USD" />
+                                </span>
+                            </h2>
+                            <button
+                                role="link"
+                                onClick={createCheckoutSession}
+                                disabled={!session}
+                                className={`button mt-2 ${!session &&
+                                    "from-gray-300 to-gray-500 border-gray-200 text-gray-300 cursor-not-allowed"
+                                    }`}>
+                                {!session ? "Sign in to checkout" : "Proceed to checkout"}
+                            </button>
+                        </>
+                    )}
                 </div>
 
             </main>
