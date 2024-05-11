5c39584a5c63707f1dd618ca75cf40c99a884208
Sergey Bolotnikov <sbolotnikov@gmail.com>
2024-05-11 01:15:05 -0400

    manifest added

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,29 @@ export default function Home({products}) {
   return (
     <div className="bg-gray-100">
       <Head>
-        <title>Bobby Store</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        <title>Bobbie Store</title>
       </Head>
       <Header/>
       <main className="max-w-screen-2xl mx-auto">
